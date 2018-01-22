665ca64c5fa8863c1ddc0631879524f791f501a3
kelloggz <32641529+kelloggz@users.noreply.github.com>
2018-01-22 14:07:26 +0100

    Add files via upload

---
+++ challenge1.py
@@ -0,0 +1,24 @@
+#############################
+#                           #
+#   Convert hex to base64   #
+#                           #
+#                           #
+#############################
+import base64
+import binascii
+
+
+def toBase(str):
+    basestr=binascii.unhexlify(str)
+    basestr=base64.b64encode(basestr)
+    return basestr
+
+string=input()
+stringtest=binascii.unhexlify(string)
+
+expected=b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+if toBase(string)==expected:
+    print("Program works")
+else:
+    print("Program does not work")
